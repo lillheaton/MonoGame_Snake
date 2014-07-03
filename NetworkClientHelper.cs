b92d242f3c9f87c6109cdf5bbc68a665f553368f
Emil Olsson <emilolsson93@gmail.com>
2014-07-03 16:47:02 +0200

    Add network client helper

---
+++ NetworkClientHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lidgren.Network;
+
+namespace MonoGameTest_V1
+{
+    public class NetworkClientHelper
+    {
+        NetClient Client { get; set; }
+        NetPeerConfiguration Config { get; set; }
+
+        public NetworkClientHelper()
+        {
+            Config = new NetPeerConfiguration("SnakeGame");
+            Config.Port = 14242;
+
+            Client = new NetClient(Config);
+        }
+    }
+}
