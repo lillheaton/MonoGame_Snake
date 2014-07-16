288681f89c4ea0ffe13bf6543f1a1e3b7a058ccf
Emil Olsson <emilolsson93@gmail.com>
2014-07-16 17:49:04 +0200

    Change name on folder Packages to NetworkPackages
    * gitignore does not allow folders named Packages (due to nuget, ask
    Emil)

---
+++ Snake.Server/NetworkPackages/BasePackage.cs
@@ -0,0 +1,13 @@
+﻿using Lidgren.Network;
+
+namespace Snake.Server.Packages
+{
+    public abstract class BasePackage
+    {
+        protected PackageType Type;
+
+        public abstract NetOutgoingMessage Encrypt(NetPeer peer);
+
+        public abstract void Decrypt(NetIncomingMessage package);
+    }
+}
---
+++ Snake.Server/NetworkPackages/HandshakePackage.cs
@@ -0,0 +1,24 @@
+﻿using Lidgren.Network;
+
+namespace Snake.Server.Packages
+{
+    public class HandshakePackage : BasePackage
+    {
+        public HandshakePackage()
+        {
+            this.Type = PackageType.Handshake;
+        }
+
+        public override NetOutgoingMessage Encrypt(NetPeer peer)
+        {
+            var package = peer.CreateMessage();
+            package.Write((byte)this.Type);
+            return package;
+        }
+
+        public override void Decrypt(NetIncomingMessage package)
+        {
+            this.Type = (PackageType)package.ReadByte();
+        }
+    }
+}
